da6bcb7ecc87a41dbdc1243d809dfafe43d8e327
j51217123 <65345958+j51217123@users.noreply.github.com>
2022-01-27 20:49:25 +0800

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDa8494DqMgmg-5XVf4Zdw6aHZUE9pg79Q",
@@ -7,5 +12,8 @@ const firebaseConfig = {
   messagingSenderId: "981356672021",
   appId: "1:981356672021:web:c42c869dca5e6d54234a1c"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export default firebaseConfig;
